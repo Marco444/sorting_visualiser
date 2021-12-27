bb7a59d1d56dd8e6c1cf8f764c14465a52996113
Marco444 <elscili444.code@gmail.com>
2021-12-27 11:28:54 -0300

    Revert "Updates"
    This reverts commit 59232381084b77485df79572378d28a7b8c0142a.

---
+++ src/visualiser/components/sliders/SliderNumberBars.js
@@ -0,0 +1,31 @@
+import {Box, Slider, Stack} from "@mui/material";
+import FormatLineSpacingIcon from "@mui/icons-material/FormatLineSpacing";
+import React from "react";
+
+export const SliderNumberBars = ({maxNumberBars, minNumberBars, defaultNumberBars,
+                                 sliderWidth, isBusy, handlerBarsNumberSlider}) => {
+   return (
+       <Stack sx={{
+           backgroundColor: 'rgba(57,92,183,0.98)',
+           marginTop: 1,
+           color: "white"
+       }}>
+           <Box id="bars-slider" sx={{
+               paddingTop: 2,
+               paddingLeft: 2,
+               display: 'flex',
+               alignItems: 'center',
+               flexWrap: 'wrap',
+               fontWeight: 'bold',
+           }} gutterBottom> Number of bars
+               <FormatLineSpacingIcon sx={{paddingLeft: 1}} />
+           </Box>
+           <Slider max={maxNumberBars} min={minNumberBars}
+                   defaultValue={defaultNumberBars} id="sliderBars" sx={{
+               marginLeft: 2,
+               marginBottom: 2,
+               width: sliderWidth * 0.825
+           }} onChange={handlerBarsNumberSlider} disabled={isBusy}/>
+       </Stack>
+   );
+}
---
+++ src/visualiser/model/algorithms/mergeSort.js
@@ -0,0 +1,55 @@
+import {addCopyAnimation} from "../animations/AnimationsEngine"
+import {getSortedAnimations} from "../animations/AnimationsEngine";
+
+export default function getMergeSortAnimations(array) {
+    const animations = [];
+    console.log('merge sort animations')
+
+    if (array.length <= 1) return array;
+
+    const auxiliaryArray = array.slice();
+
+    mergeSort(array, 0, array.length - 1, auxiliaryArray, animations);
+
+    return animations.concat(getSortedAnimations(array));
+}
+
+function mergeSort(mainArray, left, right, auxiliaryArray, animations,) {
+    if (left === right) return;
+    const middle = Math.floor((left + right) / 2);
+    mergeSort(auxiliaryArray, left, middle, mainArray, animations);
+    mergeSort(auxiliaryArray, middle + 1, right, mainArray, animations);
+    merge(mainArray, left, middle, right, auxiliaryArray, animations);
+}
+
+function merge(mainArray, left, middle, right, auxiliaryArray, animations) {
+    let k = left;
+    let i = left;
+    let j = middle + 1;
+
+
+    while (i <= middle && j <= right) {
+        if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+
+            addCopyAnimation(i, k, animations, auxiliaryArray)
+            mainArray[k++] = auxiliaryArray[i++];
+
+        } else {
+            addCopyAnimation(j, k, animations, auxiliaryArray)
+            mainArray[k++] = auxiliaryArray[j++];
+        }
+    }
+
+    while (i <= middle) {
+        addCopyAnimation(i, k, animations, auxiliaryArray)
+        mainArray[k++] = auxiliaryArray[i++];
+    }
+
+    while (j <= right) {
+        addCopyAnimation(j, k, animations, auxiliaryArray)
+        mainArray[k++] = auxiliaryArray[j++];
+    }
+
+
+
+}
